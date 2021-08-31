d07f9a431e257a2a1953aa935a9d03b6b127aa79
Mayuran-Sundermurthy <87370153+Mayuran-Sundermurthy@users.noreply.github.com>
2021-08-31 18:44:06 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,10 +17,9 @@ function preload(){
 
   eatAni = loadAnimation('eat1.png', 'eat2.png');
 
-  gymAni1 = loadAnimation('gym1.png', 'gym2.png');
-  gymAni2 = loadAnimation('gym11.png', 'gym12.png');
+  gymAni = loadAnimation('gym1.png', 'gym2.png','gym11.png','gym12.png');
 
-  moveAni = loadAnimation('move.png', 'move1.png');
+  moveAni = loadAnimation('move.png', 'move1.png', 'move.png', 'move1.png');
 
   backgroundImg = loadImage('iss.png');
 
@@ -37,6 +36,10 @@ function setup() {
   astronaut = createSprite(400,200,10,10);
   astronaut.addAnimation("sleeping",Astronaut);
   astronaut.addAnimation("moving",moveAni);
+  astronaut.addAnimation("brushing",brushImg);
+  astronaut.addAnimation("gym",gymAni);
+  astronaut.addAnimation("eat",eatAni);
+  astronaut.addAnimation("bath",bathAni);
   astronaut.scale = 0.1;
   console.log(astronaut.x);
 
@@ -45,13 +48,25 @@ function setup() {
 
 function draw() {
   background(30);
-
+  createEdgeSprites();
   if(keyDown('m')){
-    
     astronaut.changeAnimation("moving");
-    astronaut.velocityX = 1;
-    astronaut.velocityY = 1;
   }
 
+  if(keyDown(UP_ARROW)){
+    astronaut.changeAnimation("brushing");
+  }
+  
+  if(keyDown(DOWN_ARROW)){
+    astronaut.changeAnimation("gym");
+  }
+
+  if(keyDown(LEFT_ARROW)){
+    astronaut.changeAnimation("eat");
+  }
+
+  if(keyDown(RIGHT_ARROW)){
+    astronaut.changeAnimation("bath");
+  }
   drawSprites();
 }
